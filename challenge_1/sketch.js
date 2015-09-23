6502716ff68884db56128fedeb125d6c661982e7
brinnathomsen <thomb212@gmail.com>
2015-09-23 17:56:01 -0400

    Made boxes with white stroke and gradient

--- challenge_1/sketch.js
+++ challenge_1/sketch.js
@@ -10,5 +10,11 @@ function draw() {
 	colorMode(HSB, 100)
 	background(0, 0, 19);
 
+	// draw the boxes
+	for(var i = 0; i < 10; i++){
+		stroke(200, 10, 83);
+		fill(330, 100, i * 10);
+		rect(i * 60 + 25, 180, 50, 50);
+	}
 
 }
